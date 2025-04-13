e31424e85831fdb931f2c1be25722ef977bd5bc6
FrogIncorporated <johndseng@gmail.com>
2025-04-13 10:46:15 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,42 @@
-const CACHE = 'animapp-v1';
-const ASSETS = [
-  './',
+const CACHE_NAME = 'animcreator-cache-v1';
+const urlsToCache = [
   './index.html',
   './manifest.json',
   './service-worker.js',
-  // add any other static files here, e.g.:
-  // './icon-192.png',
-  // './icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
+  // Add any other assets you want to cache (e.g. external CSS or JS files)
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached asset or fetch from network.
+        return response || fetch(event.request);
+      })
+  );
 });
+
+self.addEventListener('activate', event => {
+  // Clean up any old caches.
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
